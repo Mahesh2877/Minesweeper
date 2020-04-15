13b71d3eefa408371851fa245699c930180c23d7
Mahesh S <37013197+Mahesh2877@users.noreply.github.com>
2020-04-15 20:17:00 +0530

    Update minesweeper_code.py

--- minesweeper_code.py
+++ minesweeper_code.py
@@ -2,7 +2,6 @@ def mine_sweeper(bombs, num_rows, num_cols):
     field = [[0 for i in range(num_cols)] for j in range(num_rows)]
     for i in bombs:
         field[i[0]][i[1]] = -1
-    for i in bombs:
         for x in range((i[0] - 1), (i[0] + 2), 1):
             for y in range((i[1] - 1), (i[1] + 2), 1):
                 if not((x < 0) or (x > (num_rows - 1)) or (y < 0) or (y > (num_cols - 1))):
